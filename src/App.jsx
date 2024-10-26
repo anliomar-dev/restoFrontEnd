f404c85e4585c495c386675361ecd81c4aece5a6
Anli omar <omaranli56@gmail.com>
2024-10-26 22:06:53 +0400

    fix conflict on readMe file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+    return (
+        <div className="">
+            <p className="text-amber-800">bonsoir les devs</p>
+        </div>
+  )
+}
+
+export default App
