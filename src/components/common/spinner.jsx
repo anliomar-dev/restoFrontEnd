afd3b8cd8720a7eeaf4ed73255d98597201fb42a
Anli omar <omaranli56@gmail.com>
2024-11-26 20:40:11 +0400

    display loading on first visit of the session

---
+++ src/components/common/spinner.jsx
@@ -0,0 +1,10 @@
+function Spinner() {
+	return(
+	  <div className="spinner">
+		  <span className="loading loading-ring loading-lg bg-accentHover">
+		  </span>
+	  </div>
+	)
+}
+
+export default Spinner
--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,17 +1,45 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
+import { useEffect, useState } from "react";
+import Spinner from "../components/common/spinner.jsx";
 
 function Layout() {
-  return (
-    <div className="layout-container">
-      <Navbar />
-      <main className="main-content">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  );
+    // loading is display only on first visit of the session
+    const [loading, setLoading] = useState(!sessionStorage.getItem('hasVisitedHome'));
+
+    useEffect(() => {
+        if (!sessionStorage.getItem('hasVisitedHome')) {
+            const timeoutId = setTimeout(() => {
+                setLoading(false);
+                sessionStorage.setItem('hasVisitedHome', 'true');
+            }, 3000);
+
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        } else {
+            setLoading(false);
+        }
+    }, []);
+
+    return (
+      <>
+          {loading ? (
+            <div className="fixed top-0 left-0 bg-white h-screen w-screen flex justify-center items-center">
+                <Spinner />
+            </div>
+          ) : (
+            <div className="layout-container">
+                <Navbar />
+                <main className="main-content">
+                    <Outlet />
+                </main>
+                <Footer />
+            </div>
+          )}
+      </>
+    );
 }
 
 export default Layout;
